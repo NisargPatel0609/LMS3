c55f08a06d8c8642c656fe962f5f6a8eafc37d20
Nisargkumar Naginbhai Patel <nisargkumarp@cybage.com>
2024-04-10 19:49:48 +0530

    Add project files.

---
+++ LogisticsManagement.WebAPI/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using LogisticsManagement.Service.DTOs;
+using LogisticsManagement.Service.Services.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LogisticsManagement.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        private readonly IManagerService _managerService;
+        public OrderController(IManagerService managerService)
+        {
+            _managerService = managerService;
+        }
+
+
+        [HttpGet]
+        [Route("Orders", Name = "GetAllOrders")]
+        [ProducesResponseType(200, Type = typeof(List<OrderDTO>))]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                List<OrderDTO> orders = await _managerService.GetOrders();
+                return Ok(orders);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return BadRequest();
+            }
+        }
+    }
+}
